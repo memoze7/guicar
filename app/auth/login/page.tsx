cedd58e7da83088702516506453e7ed8dea455f2
Guillermo Pérez <guillermojperezp@gmail.com>
2023-11-05 02:30:02 -0400

    create login form with inputs

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,10 @@
+import { LoginForm } from '@/components/auth/login/login-form'
+
 export default function Login () {
   return (
     <main>
      Welcome to the login page
+      <LoginForm />
     </main>
 
   )
---
+++ components/auth/login/login-form.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: 'Por favor, ingrese un email válido'
+  }),
+  password: z.string().min(6, {
+    message: 'La contraseña debe tener al menos 6 caracteres'
+  }).max(50, {
+    message: 'La contraseña debe tener menos de 50 caracteres'
+  })
+})
+
+export function LoginForm () {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit (values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input {...field} autoComplete="new-password" autoFocus={true}/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input {...field} type="password" autoComplete="new-password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
